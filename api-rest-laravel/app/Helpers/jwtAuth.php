28198b083ca3e73858162b1cd8cb0f7b13feca28
Kike <kikumen91@gmail.com>
2019-11-21 16:58:35 +0100

    parte de autenticacion terminada

--- api-rest-laravel/app/Helpers/jwtAuth.php
+++ api-rest-laravel/app/Helpers/jwtAuth.php
@@ -7,21 +7,51 @@ use Illuminate\Support\Facades\DB;
 use App\User;
 
 class jwtAuth{
-    public function signUp(){
-        // Buscar al usuario con sus credenciales
 
+    public $key;
+
+    public function __construct(){
+        $this->key = 'key_para_token_crypt0_2o19';
+    }
+
+    public function signUp($email, $password){
+        // Buscar al usuario con sus credenciales
+        $user = User::where([
+            'email' => $email,
+            'password' =>$password
+        ])->first();
 
         //Comprobar si las credenciales son correctas
-
+        $signUp = false;
+        if(\is_object($user)){
+            $signUp = true;
+        }
 
         //Generar el token con los datos del usuario
+        if($signUp){
 
+            $token = array(
+                'sub'  => $user->id,
+                'email'  => $user->email,
+                'name'  => $user->name,
+                'surname'  => $user->surname,
+                'iat'  => time(),
+                'exp' => time() + (7 * 24 * 60 * 60)
+            );
+
+            $jwt = JWT::encode($token, $this->key, 'HS256');
+        } else {
+            $data = array(
+                'status' => 'error'
+            );
+        }
 
         //Devolver los datos codificados o el token en funcion de un parametro
 
 
         return 'Metodo de la clase JWTAUTH. ';
     }
+
 }
 
 
