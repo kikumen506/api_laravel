5c67316256578c4de7923a2bbf9c8ce816ff7198
Kike <kikumen91@gmail.com>
2019-11-21 09:30:36 +0100

    modelos creados, base de datos creada, y enlazada

--- api-rest-laravel/app/User.php
+++ api-rest-laravel/app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    
+    //Relacion de uno a muchos.
+    public function posts(){
+        return $this->hasMany('App\Post');
+
+    }
 }
--- api-rest-laravel/routes/web.php
+++ api-rest-laravel/routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pruebas/{nombre?}', function($nombre = null){
+    $texto = '<h2>Texto desde una ruta</h2>';
+    $texto .= 'Nombre: '.$nombre;
+    return view('pruebas', array(
+        'texto' => $texto
+    ));
+});
+
+
+Route::get('/animales', 'PruebasController@index');
+Route::get('/test-orm', 'PruebasController@testOrm');
